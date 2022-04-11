e17d13c3d3051b38ab72476ff0c4353757bc23ca
John Little <john.little@tufts.edu>
2022-04-11 17:23:25 -0400

    little nitpicky things that might be helpful -- won't mind if you dont agree :)

--- option1/cpp/hashing_OA.cpp
+++ option1/cpp/hashing_OA.cpp
@@ -1,3 +1,25 @@
 #include "hashing_OA.h"
 
+#include <stdexcept>
+
+using namespace std;
+
 // Implement hashing with open addressing
+
+HashtableOA::HashtableOA() {
+    // TODO
+}
+
+HashtableOA::~HashtableOA() {
+    // TODO
+}
+
+void HashtableOA::insert(int x) {
+	(void)x;
+    throw runtime_error("HashtableOA::insert is not implemented.");
+}
+
+bool HashtableOA::query(int x) {
+    (void)x;
+    throw runtime_error("HashtableOA::query is not implemented.");
+}
